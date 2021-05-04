a3805c2f04e6f1ec8f99b43efb35a3a48eb503e0
skyfall947 <atencio947@gmail.com>
2021-05-03 21:25:33 -0400

    modularizacion de los endpoints de blog

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 'use strict';
-
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 
 const config = require('./utils/config');
-const Blog = require('./models/blog');
+const blogRouter = require('./controllers/blog');
 
 mongoose.connect(config.MONGODB_URI, {
   useNewUrlParser: true,
@@ -18,22 +17,7 @@ mongoose.connect(config.MONGODB_URI, {
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/blogs', (req, res) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      res.json(blogs);
-    });
-});
-
-app.post('/api/blogs', (req, res) => {
-  const blog = new Blog(req.body);
-
-  blog.save()
-    .then(result => {
-      res.status(201).json(result);
-    });
-});
+app.use('/api/blog', blogRouter);
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`);
