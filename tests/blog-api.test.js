5da29ccd2e4cec712fbb2ba5bc01c218b4b2a3c8
skyfall947 <atencio947@gmail.com>
2021-05-11 13:25:47 -0400

    4.22*: expansión de la lista de blogs, paso 10

--- tests/blog-api.test.js
+++ tests/blog-api.test.js
@@ -3,17 +3,35 @@ const supertest = require('supertest');
 const bcrypt = require('bcrypt');
 
 const app = require('../app');
+const helper = require('./test_helper');
 const Blog = require('../models/blog');
 const User = require('../models/user');
-const helper = require('./test_helper');
 
 const api = supertest(app);
 
+let jwt;
 beforeEach(async() => {
+  const idListUsers = [];
+  await User.deleteMany({});
+  for (let i = 1; i <= 6; i++) {
+    const passwordHash = await bcrypt.hash(`Abc@${i}`, 10);
+    const user = new User({ username: `user${i}`, passwordHash });
+    idListUsers.push((await user.save())._id);
+  }
   await Blog.deleteMany({});
-  const blogObjects = helper.initialBlogs.map(blog => new Blog(blog));
+  const blogObjects = helper.initialBlogs.map((blog, i) => {
+    return new Blog({ ...blog, user: idListUsers[i] });
+  });
   const promiseArray = blogObjects.map(blog => blog.save());
   await Promise.all(promiseArray);
+
+  const userForLogin = {
+    username: 'user2',
+    password: 'Abc@2'
+  };
+  const response = await api.post('/api/login').send(userForLogin).expect(200);
+  expect(response.body.username).toBe(userForLogin.username);
+  jwt = response.body.token;
 });
 
 describe('/api/blogs', () => {
@@ -36,10 +54,14 @@ describe('/api/blogs', () => {
       url: 'https://cleancode.com/',
       likes: 10
     };
-    await api.post('/api/blogs')
+
+    await api
+      .post('/api/blogs')
+      .set('Authorization', `bearer ${jwt}`)
       .send(newBlog)
       .expect(201)
       .expect('Content-Type', /application\/json/);
+
     const blogsAtEnd = await helper.blogsInDB();
     expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
     const titles = blogsAtEnd.map(blog => blog.title);
@@ -53,7 +75,9 @@ describe('/api/blogs', () => {
         author: 'Robert Martin',
         url: 'https://cleancode.com/'
       };
-      const response = await api.post('/api/blogs').send(newBlog);
+      const response = await api.post('/api/blogs')
+        .set('Authorization', `bearer ${jwt}`)
+        .send(newBlog);
       expect(response.body.likes).toBe(0);
     });
 
@@ -63,27 +87,73 @@ describe('/api/blogs', () => {
         likes: 10,
         url: 'https://somethig.com'
       };
-      await api.post('/api/blogs').send(newBlog).expect(400);
-    });
+      await api.post('/api/blogs')
+        .set('Authorization', `bearer ${jwt}`)
+        .send(newBlog)
+        .expect(400);
+    }
+  );
 
-  test('DELETE - succeed with a valid ID', async() => {
-    const [blogToDelete] = await helper.blogsInDB();
-    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
-    const blogsAtEnd = await helper.blogsInDB();
-    expect(blogsAtEnd).toHaveLength(
-      helper.initialBlogs.length - 1
-    );
-    const titles = blogsAtEnd.map(blog => blog.title);
-    expect(titles).not.toContain(blogToDelete.title);
+  test('POST - failed with status 401 when the token is not provided',
+    async() => {
+      const newBlog = {
+        title: 'Clean Code',
+        author: 'Robert Martin',
+        url: 'https://cleancode.com/',
+        likes: 15
+      };
+      await api.post('/api/blogs').send(newBlog)
+        .expect(401);
+    }
+  );
+});
+
+describe('/api/blogs - test with the owner of the blog', () => {
+  let jwtUser5;
+  let blogToOperate;
+  beforeEach(async() => {
+    // create a blog with a specific user
+    // 1. login with a specific user
+    const userToLogin = { username: 'user5', password: 'Abc@5' };
+    const response = await api.post('/api/login')
+      .send(userToLogin).expect(200);
+    jwtUser5 = response.body.token;
+    // 2. create a blog with that user
+    const newBlog = {
+      title: 'JS the weird parts',
+      author: 'Somebody',
+      url: 'http://blog.com',
+      likes: 1,
+    };
+    const respBlog = await api.post('/api/blogs').send(newBlog)
+      .set('Authorization', `bearer ${jwtUser5}`)
+      .expect(201);
+    blogToOperate = respBlog.body;
   });
+
+  test('DELETE - succeed with a valid ID & valid owner token',
+    async () => {
+      const blogsAtStart = await helper.blogsInDB();
+      await api.delete(`/api/blogs/${blogToOperate.id}`)
+        .set('Authorization', `bearer ${jwtUser5}`)
+        .expect(204);
+      const blogsAtEnd = await helper.blogsInDB();
+      expect(blogsAtEnd).toHaveLength(blogsAtStart.length - 1);
+
+      const titles = blogsAtEnd.map(blog => blog.title);
+      expect(titles).not.toContain(blogToOperate.title);
+    }
+  );
 
   test('DELETE - failed with status 400 if id is not valid',
     async () => {
-      await api.delete('/api/blogs/aabbcc').expect(400);
+      await api.delete('/api/blogs/aabbcc')
+        .set('Authorization', `bearer ${jwtUser5}`)
+        .expect(400);
     });
 
   test('PUT - succeed with a valid ID and data provided',
-    async() => {
+    async () => {
       const [blogToUpdate] = await helper.blogsInDB();
       blogToUpdate.likes = 14;
       await api.put(`/api/blogs/${blogToUpdate.id}`)
@@ -98,14 +168,6 @@ describe('/api/blogs', () => {
 });
 
 describe('/api/users', () => {
-  beforeEach(async () => {
-    await User.deleteMany({});
-    for (let i = 1; i <= 5; i++) {
-      const passwordHash = await bcrypt.hash(`Abc@${i}`, 10);
-      const user = new User({ username: `user${i}`, passwordHash });
-      await user.save();
-    }
-  });
   test('new user added succeeded', async () => {
     const usersAtStart = await helper.usersInDB();
     const newUser = {
